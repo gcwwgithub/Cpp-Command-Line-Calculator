72d0db02e4c471806698ec1f9337444336ed8e93
gcwwgithub <30390539+gcwwgithub@users.noreply.github.com>
2025-03-04 23:55:43 +0800

    Added history feature

--- CppCommandLineCalculator/Calculator.h
+++ CppCommandLineCalculator/Calculator.h
@@ -7,15 +7,31 @@
 #include <sstream>
 #include <exception>
 
+
+//determines the result of the expression
+enum class ResultType 
+{
+	Success,
+    History,
+	Failure
+};
+
+
 template <typename T>
 class Calculator 
 {
 
 private:
-    std::unordered_map<char, std::unique_ptr<Operation<T>>> operations; // Store pointers
+
+    //stores pointers to the operations
+    std::unordered_map<char, std::unique_ptr<Operation<T>>> operations;
+
     T leftOperand = 0;
     T rightOperand = 0;
     char operation = '\0';
+
+    //used for the history feature
+    std::stack<std::string> expressionStack;
 
 public:
 
@@ -29,6 +45,7 @@ public:
         leftOperand = rightOperand = T();
     }
 
+    //Calculates the result of the expression
     T Compute() 
     {
         if (operations.count(operation) == 0)
@@ -39,7 +56,24 @@ public:
 
         try 
         {
-            return operations.at(operation)->Calculate(leftOperand, rightOperand); // Use `->` for pointers
+
+            T value = operations.at(operation)->Calculate(leftOperand, rightOperand);
+            
+            //converts the result to a long long if it is a whole number
+            long long leftOperandConverted = static_cast<long long>(leftOperand);
+            long long rightOperandConverted = static_cast<long long>(rightOperand);
+            long long valueConverted = static_cast<long long>(value);
+
+            //converts the result to a string
+            std::string leftOperandString = (leftOperand == leftOperandConverted) ? std::to_string(leftOperandConverted) : std::to_string(leftOperand);
+            std::string rightOperandString = (rightOperand == rightOperandConverted) ? std::to_string(rightOperandConverted) : std::to_string(rightOperand);
+            std::string valueString = (value == valueConverted) ? std::to_string(valueConverted) : std::to_string(value);
+
+            std::string expression = leftOperandString + " " + operation + " " + rightOperandString + " = " + valueString;
+
+            //pushes the expression to the stack
+            expressionStack.push(expression);
+            return value;
         }
 
         catch (const std::exception& e) 
@@ -51,7 +85,9 @@ public:
       
     }
 
-    bool ParseExpression(const std::string& input) 
+
+    //Parses the input and seperates the operands and operator
+    ResultType ParseExpression(const std::string& input)
     {
         std::string temp1, temp2;
         char tempOperator = '\0';
@@ -64,7 +100,18 @@ public:
         {
             tokens.push_back(token);
         }
-        std::cout << input << " " << tokens.size() << std::endl;
+
+
+        //cheks if the input is "h" or "H" and returns the history
+        if(tokens.size() == 1)
+		{
+			temp1 = tokens[0];
+            if (temp1 == "h" || temp1 == "H")
+            {
+                return ResultType::History;
+            }
+		
+		}
 
 
         if (tokens.size() == 3) 
@@ -74,12 +121,13 @@ public:
             temp2 = tokens[2];
         }
 
+        //the user may haved entered the expression without spaces
         else {
             bool foundOperator = false;
             for (char c : input) 
             {
-                std::cout << c << " " << std::endl;
 
+                //help craft the operands and operator
                 if (std::isdigit(c) || c == '.') 
                 {
                     if (!foundOperator) 
@@ -97,8 +145,8 @@ public:
                 {
                     if (foundOperator)
                     {
-                        std::cout << "Error: Multiple operators detected in input.\n";
-                        return false;
+                        std::cout << "Error: Multiple operators detected in input." << std::endl;
+                        return ResultType::Failure;
                     }
 
                     tempOperator = c;
@@ -107,22 +155,24 @@ public:
 
                 else 
                 {
-                    std::cout << "Error: Invalid character detected: " << c << "\n";
-                    return false;
+                    std::cout << "Error: Invalid character detected: " << c << std::endl;
+                    return ResultType::Failure;
                 }
             }
         }
 
+        //checks if the operator is missing
         if (tempOperator == '\0')
         {
-            std::cout << "Error: No operator found in input.\n";
-            return false;
+            std::cout << "Error: No operator found in input." << std::endl;
+            return ResultType::Failure;
         }
 
+        //checks if the operand is missing
         if (temp1.empty()) 
         {
-            std::cout << "Error: Missing first operand.\n";
-            return false;
+            std::cout << "Error: Missing first operand." << std::endl;
+            return ResultType::Failure;
         }
 
         try 
@@ -136,19 +186,31 @@ public:
 
             else 
             {
-                std::cout << "Error: Missing second operand.\n";
-                return false;
+                std::cout << "Error: Missing second operand." << std::endl;
+                return ResultType::Failure;
             }
 
             operation = tempOperator;
-            return true;
+            return ResultType::Success;
         }
 
         catch (const std::exception& e) 
         {
-            std::cout << "Error: Invalid number format " << e.what();
-            return false;
+            std::cout << "Error: Invalid number format " << e.what() << std::endl;
+            return ResultType::Failure;
         }
     }
+
+    std::string RetrievePreviousExpression() 
+	{
+		if (expressionStack.empty()) 
+		{
+			return "Empty";
+		}
+
+		std::string expression = expressionStack.top();
+		expressionStack.pop();
+		return expression;
+	}
 
 };;
--- CppCommandLineCalculator/main.cpp
+++ CppCommandLineCalculator/main.cpp
@@ -5,24 +5,41 @@
 
 int main() 
 {
-	std::cout << "Hello World!" << std::endl;
+
 
 	Calculator<double> calc;
+	std::cout << "Hello! Welcome to the Calculator. To use the calculator, type in an expression (Example: 18 + 5) \n" << std::endl;
+	std::cout << "Operations allowed: Plus(+), Minus(-), Multiplication(*), Division(/), Exponential(^) \n" << std::endl;
+	std::cout << "You may type in h to see the previous expression\n" << std::endl;
 
 	while (1) {
 		std::string expression;
-		std::cout << "Enter an expression: ";
+		std::cout << "Enter the expression: ";
 		std::getline(std::cin, expression);
 
-		if (calc.ParseExpression(expression))
+
+		switch (calc.ParseExpression(expression))
 		{
-			double value = calc.Compute();
-			std::cout << "Result: " << value << std::endl;
+			case ResultType::Success:
+			{
+				double value = calc.Compute();
+				std::cout << "Result: " << value << std::endl;
+				break;
+			}
+			case ResultType::History:
+			{
+				std::cout << "History: " << calc.RetrievePreviousExpression() << std::endl;
+				break;
+			}
+			case ResultType::Failure:
+			{
+				std::cout << "Error: Invalid expression.\n";
+				break;
+			}
+
 		}
-		else
-		{
-			std::cerr << "Error: Invalid expression.\n";
-		}
+
+
 	}
 	
 
